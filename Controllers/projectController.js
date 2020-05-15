75589e4093bfc6408fe6cd7902c7ff8bf08eba11
mikiPP <miki.palao.palmer@gmail.com>
2020-05-15 16:57:29 +0200

    refactor(Project): Project controller has been refactored

--- Controllers/projectController.js
+++ Controllers/projectController.js
@@ -1,130 +1,117 @@
-const mongoose = require('mongoose');
-
 const Project = require('../Models/project');
+const checkIfIdIsValid = require('../Util/utils').checkIfIdIsValid;
 
 exports.getProjectById = async (req, res, next) => {
-  try {
-    const { projectId } = req.params;
+	try {
+		const { projectId } = req.params;
 
-    const project = await Project.findById(projectId);
+		const project = await Project.findById(projectId);
 
-    if (!project) {
-      const error = new Error(
-        `Coudn't not find project by the id: ${projectId}`
-      );
-      error.statusCode = 404;
-      throw error;
-    }
-    res.status(200).json({ message: 'Project have been fetched', project });
-    return project;
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-    return err;
-  }
+		if (!project) {
+			const error = new Error(
+				`Coudn't not find project by the id: ${projectId}`
+			);
+			error.statusCode = 404;
+			throw error;
+		}
+		res.status(200).json({ message: 'Project have been fetched', project });
+		return project;
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+		return err;
+	}
 };
 
 exports.addProject = async (req, res, next) => {
-  const { name } = req.body;
-  const { scrumMaster } = req.body;
-  const { disabled } = req.body;
-  const { companyId } = req.body;
+	const { name } = req.body;
+	const { scrumMaster } = req.body;
+	const { disabled } = req.body;
+	const { companyId } = req.body;
 
-  const project = new Project({ name, scrumMaster, disabled, companyId });
+	const project = new Project({ name, scrumMaster, disabled, companyId });
 
-  try {
-    const result = await project.save();
-    if (!result) {
-      const error = new Error('The project has not been created');
-      error.statusCode = 500;
-      throw error;
-    }
+	try {
+		const result = await project.save();
+		if (!result) {
+			const error = new Error('The project has not been created');
+			error.statusCode = 500;
+			throw error;
+		}
 
-    res.status(201).json({ message: 'project created!', Project: result });
-    return result;
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-    return err;
-  }
+		res.status(201).json({ message: 'project created!', Project: result });
+		return result;
+	} catch (err) {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+		return err;
+	}
 };
 
 exports.updateProject = (req, res, next) => {
-  const { projectId } = req.params;
+	const { projectId } = req.params;
 
-  const { name } = req.body;
-  const { scrumMaster } = req.body;
-  const { disabled } = req.body;
-  const { companyId } = req.body;
+	const { name } = req.body;
+	const { scrumMaster } = req.body;
+	const { disabled } = req.body;
+	const { companyId } = req.body;
 
-  if (mongoose.Types.ObjectId.isValid(projectId)) {
-    Project.findById(projectId)
-      .then(project => {
-        if (project) {
-          project.name = name;
-          project.scrumMaster = scrumMaster;
-          project.disabled = disabled;
-          project.companyId = companyId;
 
-          return project.save();
-        }
-        const error = new Error(
-          `Project with id: ${projectId} has not been found!`
-        );
-        error.statusCode = 404;
-        throw error;
-      })
-      .then(result => {
-        res.status(201).json({ message: 'Project updated!', project: result });
-      })
-      .catch(err => {
-        if (!err.statusCode) {
-          err.statusCode = 500;
-        }
-        return next(err);
-      });
-  } else {
-    const error = new Error(`this id: ${projectId} is invalid!`);
-    error.statusCode = 422;
-    res.statusCode = 422; // just for testing
-    return next(error);
-  }
+	checkIfIdIsValid(projectId,res,next);
+	Project.findById(projectId)
+		.then(project => {
+			if (project) {
+				project.name = name;
+				project.scrumMaster = scrumMaster;
+				project.disabled = disabled;
+				project.companyId = companyId;
+
+				return project.save();
+			}
+			const error = new Error(
+				`Project with id: ${projectId} has not been found!`
+			);
+			error.statusCode = 404;
+			throw error;
+		})
+		.then(result => {
+			res.status(201).json({ message: 'Project updated!', project: result });
+		})
+		.catch(err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			return next(err);
+		});
 };
 
 exports.deleteProject = (req, res, next) => {
-  const { projectId } = req.params;
+  
+	const { projectId } = req.params;
 
-  if (mongoose.Types.ObjectId.isValid(projectId)) {
-    // console.log(Project.findByIdAndDelete());
-    Project.findByIdAndDelete(projectId)
-      .then(result => {
-        if (result) {
-          res.status(200).json({
-            message: `project with id: ${projectId} has been deleted`,
-          });
-          return;
-        }
-        const error = new Error(
-          `Project with id: ${projectId} has not been found!`
-        );
-        error.statusCode = 404;
-        throw error;
-      })
-      .catch(err => {
-        console.log('Here?');
-        if (!err.statusCode) {
-          err.statusCode = 500;
-        }
-        return next(err);
-      });
-  } else {
-    const error = new Error(`this id: ${projectId} is invalid!`);
-    error.statusCode = 422;
-    res.statusCode = 422; // just for testing
-    return next(error);
-  }
+	checkIfIdIsValid(projectId,res,next);
+	Project.findByIdAndDelete(projectId)
+		.then(result => {
+			if (result) {
+				res.status(200).json({
+					message: `project with id: ${projectId} has been deleted`,
+				});
+				return;
+			}
+			const error = new Error(
+				`Project with id: ${projectId} has not been found!`
+			);
+			error.statusCode = 404;
+			throw error;
+		})
+		.catch(err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			return next(err);
+		});
 };
